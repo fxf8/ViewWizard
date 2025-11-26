f02a114b5723f267d50c39ac94aca21f1ba9f566
Fuexfollets <fuex9follets@gmail.com>
2025-11-26 15:11:16 -0500

    Added to serialization, implemented id searching method in session

--- viewwizard/session.py
+++ viewwizard/session.py
@@ -1,7 +1,10 @@
 import pathlib
 import pickle
+import random
+from typing import cast
 import uuid
 
+import googleapiclient.discovery as discovery
 import torch
 
 import viewwizard.database as db
@@ -19,12 +22,14 @@ class ProgramSession:
             vmodel.ModelTrainingHistory,
         ]
     ]
+    client: discovery.Resource | None
 
     def __init__(self):
         self.session_id = uuid.uuid4()
 
         self.dataset = []
         self.models = []
+        self.client = None
 
     def save_to_path(self, path: pathlib.Path):
         model_states: list[dict] = []
@@ -42,8 +47,10 @@ class ProgramSession:
             training_histories.append(history)
 
         temporary_models = self.models
+        temporary_client = self.client
 
         self.models = []
+        self.client = None
 
         with path.open("wb") as file:
             pickle.dump(
@@ -58,7 +65,9 @@ class ProgramSession:
                 ),
                 file,
             )
+
         self.models = temporary_models
+        self.client = temporary_client
 
     @classmethod
     def load_from_path(cls, path: pathlib.Path) -> "ProgramSession":
@@ -88,4 +97,47 @@ class ProgramSession:
 
             session.models.append((model, optimizer, history))
 
+        session.client = None
+
         return session
+
+    def ensure_client(self) -> discovery.Resource:
+        resource = self.client
+
+        if resource is None:
+            resource = cast(
+                discovery.Resource,
+                discovery.build("youtube", "v3", cache_discovery=False),
+            )
+
+        self.client = resource
+
+        return resource
+
+    def create_dataset(self, name: str):
+        self.dataset.append((name, db.VideoDataset()))
+
+    def search_new_thumbnails(
+        self, dataset_number: int, search_count: int
+    ) -> int | None:
+        if dataset_number >= len(self.dataset):
+            return None
+
+        client: discovery.Resource = self.ensure_client()
+
+        _, dataset = self.dataset[dataset_number]
+
+        new_ids: list[str] = sum(
+            [
+                db.sample_random_search_ids(
+                    client, db.create_random_query(random.randint(2, 4))
+                )
+                for _ in range(search_count)
+            ],
+            [],
+        )
+
+        dataset.add_ids(new_ids)
+        dataset.sync_id_video_data(client)
+
+        return len(new_ids)
